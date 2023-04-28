177669fb92bd88ab665f95e7cd5e9448a57dedfe
Eliazar Suarez <eliazarsuarez08@gmail.com>
2023-04-28 15:24:34 -0300

    primer commit

---
+++ OdontoEli/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OdontoEli.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ OdontoEli/Models/Patient.cs
@@ -0,0 +1,12 @@
+namespace OdontoEli.Models;
+public class Patient
+{
+    public int IdPac { get; set; }
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public string Adress { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+    public int CellPhone { get; set; }
+}
+
